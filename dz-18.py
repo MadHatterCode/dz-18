1aab86f732b7c028e71a911c713cd100f6262bce
Sergii <sadante@gmail.com>
2024-03-09 12:35:44 +0200

    updated

--- dz-18.py
+++ dz-18.py
@@ -1,8 +1,7 @@
 def common_elements():
     l1 = [num for num in range(1, 100) if num % 3 == 0]
     l2 = [num for num in range(1, 100) if num % 5 == 0]
-    return print(set(l1).union(set(l2)))
+    return set(l1).union(set(l2))
+
 
 common_elements()
-
-
