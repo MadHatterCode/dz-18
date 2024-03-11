39dcbcf06cddaece25d2c2860acb733151f3368e
Sergii <sadante@gmail.com>
2024-03-11 17:56:28 +0200

    updated correct method

--- dz-18.py
+++ dz-18.py
@@ -1,10 +1,10 @@
 def common_elements():
     l1 = [num for num in range(1, 100) if num % 3 == 0]
     l2 = [num for num in range(1, 100) if num % 5 == 0]
-    return set(l1).union(set(l2))
+    return set(l1).intersection(set(l2))
 
 
-common_elements()
+print(common_elements())
 
-assert isinstance(common_elements(), set) == True, "Test 1"
+assert isinstance(common_elements(), set), "Test 1"
 print("OK")
